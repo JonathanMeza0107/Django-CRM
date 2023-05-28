430855914989247a025eac65a8ced69c2571bfb0
JonathanMeza0107 <mbjonathan@soy.sena.edu.co>
2023-05-28 14:28:34 -0500

    config settings for deploy on render.com

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 set -o errexit
 
 # poetry install
-pip install --upgrade pip
+# python -m pip install --upgrade pip
+# pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
